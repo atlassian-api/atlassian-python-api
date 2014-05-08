fb1fd3fecef42dd8ed024e1fff32d7389cc43e15
Matt Harasymczuk <matt@harasymczuk.pl>
2014-05-08 14:07:14 +0200

    PYAPI-16 Jira: JQL

--- atlassian/jira.py
+++ atlassian/jira.py
@@ -8,3 +8,6 @@ class Jira(Atlassian):
 
     def reindex(self):
         return self.post("/rest/api/2/reindex")
+
+    def jql(self, jql):
+        return self.get("/rest/api/2/search?jql={0}&maxResults=999999".format(jql))
