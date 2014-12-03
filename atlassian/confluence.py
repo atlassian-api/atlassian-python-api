3258e4883daf4ab2c60e1d5a43172c7b2d79868b
Matt Harasymczuk <matt@harasymczuk.pl>
2014-12-03 17:39:58 +0100

    PYAPI-15 Confluence: Create Page

--- atlassian/confluence.py
+++ atlassian/confluence.py
@@ -3,10 +3,11 @@ from atlassian import Atlassian
 
 class Confluence(Atlassian):
 
-    def create_page(self, space, title, body):
+    def create_page(self, space, parent_id, title, body):
         return self.post("/rest/api/content/", {
             "type": "page",
             "title": title,
+            "ancestors": [{"type": "page", "id": parent_id}],
             "space": {"key": space},
             "body": {"storage": {
                 "value": body,
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="atlassian-python-api",
     description="Atlassian Python API",
     license="Apache License 2.0",
-    version="0.3.1",
+    version="0.3.3",
     download_url="https://github.com/MattAgile/atlassian-python-api",
 
     author="Matt Harasymczuk",
