7e102e9703cd7830a893f3ec1d5a3aee7a23986b
Matt Harasymczuk <matt@harasymczuk.pl>
2015-03-10 15:42:56 +0100

    PYAPI-23 Common: More Verbose Logging When HTTP Status != 200

--- atlassian/__init__.py
+++ atlassian/__init__.py
@@ -15,15 +15,15 @@ class AtlassianRestAPI:
         self.username = username
         self.password = password
 
-    def log_curl_debug(self, method, path, headers={}, data=None):
-        command = "curl --silent -X {method} -u '{username}':'{password}' -H {headers} {data} '{url}'".format(
+    def log_curl_debug(self, method, path, headers={}, data=None, level=logging.DEBUG):
+        message = "curl --silent -X {method} -u '{username}':'{password}' -H {headers} {data} '{url}'".format(
             method=method,
             username=self.username,
             password=self.password,
             headers=' -H '.join(["'{0}: {1}'".format(key, value) for key, value in headers.items()]),
             data='' if not data else "--data '{0}'".format(json.dumps(data)),
             url='{0}{1}'.format(self.url, path))
-        log.debug(command)
+        log.log(level=level, msg=message)
 
     def request(self, method='GET', path='/', headers={'Content-Type': 'application/json', 'Accept': 'application/json'}, data=None):
         self.log_curl_debug(method, path, headers, data)
@@ -34,6 +34,9 @@ class AtlassianRestAPI:
             data=json.dumps(data),
             auth=(self.username, self.password),
             timeout=60)
+        if response.status_code != 200:
+            self.log_curl_debug(method, path, headers, data, level=logging.WARNING)
+            log.warning(response.json())
         response.raise_for_status()
         log.debug('Received: {0}'.format(response.json()))
         return response
