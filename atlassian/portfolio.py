2144a1ff4da6738b1ac9a75442158df1032a6446
Matt Harasymczuk <matt@harasymczuk.pl>
2015-03-16 13:04:48 +0100

    PYAPI-27 Portfolio: Cleanup in API

--- atlassian/portfolio.py
+++ atlassian/portfolio.py
@@ -7,46 +7,62 @@ log = logging.getLogger('atlassian.portfolio')
 
 class Portfolio(AtlassianRestAPI):
 
-    def plan(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}.json'.format(plan_id))
+    def __init__(self, plan_id, *args, **kwargs):
+        self.plan_id = plan_id
+        super(Portfolio, self).__init__(*args, **kwargs)
 
-    def stages(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/stages.json'.format(plan_id))
+    def get_epic(self, epic):
+        key = [x.get('link', None) for x in epic.get('links', [])]
+        estimates = self.estimates_dict(epic['estimates'])
+        estimates.update(Total=sum(estimates.values()))
+        return {
+            'title': epic.get('title', None),
+            'team': self.team_name(epic.get('teamId')) if epic.get('teamId', None) else None,
+            'description': epic.get('description', None),
+            'issuekey': key[0] if key else None,
+            'estimates': estimates}
 
-    def teams(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/teams.json'.format(plan_id))
+    def plan(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}.json'.format(self.plan_id))
 
-    def team_name(self, plan_id, team_id):
-        return [team['title'] for team in self.teams(team_id)['collection'] if team['id'] == str(team_id)][0]
+    def stages(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/stages.json'.format(self.plan_id))
 
-    def config(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/config.json'.format(plan_id))
+    def teams(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/teams.json'.format(self.plan_id))
 
-    def persons(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/persons.json'.format(plan_id))
+    def team_name(self, team_id):
+        return [team['title'] for team in self.teams()['collection'] if team['id'] == str(team_id)][0]
 
-    def streams(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/streams.json'.format(plan_id))
+    def config(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/config.json'.format(self.plan_id))
 
-    def releases(self, plan_id):
-        return self.streams(plan_id)
+    def persons(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/persons.json'.format(self.plan_id))
 
-    def themes(self, plan_id):
-        return self.get('/rest/roadmap/1.0/plans/{0}/themes.json'.format(plan_id))
+    def streams(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/streams.json'.format(self.plan_id))
 
-    def state(self, plan_id, plan_version):
-        return self.get('/rest/roadmap/1.0/scheduling/{0}/state.json?planVersion={1}'.format(plan_id, plan_version))
+    def releases(self):
+        return self.streams()
 
-    def filter(self, plan_id, plan_version, limit=500):
-        url = '/rest/roadmap/1.0/plans/{0}/workitems/filter.json?planVersion={1}'.format(plan_id, plan_version)
+    def themes(self):
+        return self.get('/rest/roadmap/1.0/plans/{0}/themes.json'.format(self.plan_id))
+
+    def state(self, plan_version):
+        return self.get('/rest/roadmap/1.0/scheduling/{0}/state.json?planVersion={1}'.format(self.plan_id,
+                                                                                             plan_version))
+
+    def filter(self, plan_version, limit=500):
+        url = '/rest/roadmap/1.0/plans/{0}/workitems/filter.json?planVersion={1}'.format(self.plan_id, plan_version)
         return self.post(url, data={'limit': limit})
 
     def dependencies(self, workitem_id, plan_version):
         url = '/rest/roadmap/1.0/workitems/{0}/dependencies.json?planVersion={1}'.format(workitem_id, plan_version)
         return self.get(url)
 
-    def stage_name(self, plan_id, stage_id):
-        return [stage['title'] for stage in self.stages(plan_id)['collection'] if stage['id'] == str(stage_id)][0]
+    def stage_name(self, stage_id):
+        return [stage['title'] for stage in self.stages()['collection'] if stage['id'] == str(stage_id)][0]
 
-    def estimates_dict(self, plan_id, estimates):
-        return {self.stage_name(plan_id, stage['targetId']): stage['value'] for stage in estimates['stages']}
+    def estimates_dict(self, estimates):
+        return {self.stage_name(stage['targetId']): stage['value'] for stage in estimates['stages']}
