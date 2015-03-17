53aac1847aad875cced4201f39d6d02b6b820ad7
Matt Harasymczuk <matt@harasymczuk.pl>
2015-03-17 16:06:51 +0100

    PYAPI-29 Portfolio: Import Issues and Search Filter

--- atlassian/portfolio.py
+++ atlassian/portfolio.py
@@ -23,39 +23,61 @@ class Portfolio(AtlassianRestAPI):
             'estimates': estimates}
 
     def plan(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}.json'.format(self.plan_id)
+        return self.get(url)
 
     def stages(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/stages.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/stages.json'.format(self.plan_id)
+        return self.get(url)
 
     def teams(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/teams.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/teams.json'.format(self.plan_id)
+        return self.get(url)
 
     def team_name(self, team_id):
         return [team['title'] for team in self.teams()['collection'] if team['id'] == str(team_id)][0]
 
     def config(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/config.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/config.json'.format(self.plan_id)
+        return self.get(url)
 
     def persons(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/persons.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/persons.json'.format(self.plan_id)
+        return self.get(url)
 
     def streams(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/streams.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/streams.json'.format(self.plan_id)
+        return self.get(url)
 
     def releases(self):
         return self.streams()
 
     def themes(self):
-        return self.get('/rest/roadmap/1.0/plans/{0}/themes.json'.format(self.plan_id))
+        url = '/rest/roadmap/1.0/plans/{0}/themes.json'.format(self.plan_id)
+        return self.get(url)
 
     def state(self, plan_version):
-        return self.get('/rest/roadmap/1.0/scheduling/{0}/state.json?planVersion={1}'.format(self.plan_id,
-                                                                                             plan_version))
+        url = '/rest/roadmap/1.0/scheduling/{0}/state.json?planVersion={1}'.format(self.plan_id, plan_version)
+        return self.get(url)
 
     def filter(self, plan_version, limit=500):
         url = '/rest/roadmap/1.0/plans/{0}/workitems/filter.json?planVersion={1}'.format(self.plan_id, plan_version)
         return self.post(url, data={'limit': limit})
+
+    def filters(self, query_string):
+        url = '/rest/roadmap/1.0/system/filters.json?queryString={0}'.format(query_string)
+        return self.get(url)
+
+    def import_issues(self, jql, limit=100):
+        url = '/rest/roadmap/1.0/system/import.json'
+        data = {'planId': str(self.plan_id),
+                'query': jql,
+                'excludeLinked': False,
+                'epicFetchEnabled': True,
+                'maxResults': limit,
+                'estimationMethod': 'estimates',
+                'loadStoryPoints': True}
+        self.post(url, data=data)
 
     def dependencies(self, workitem_id, plan_version):
         url = '/rest/roadmap/1.0/workitems/{0}/dependencies.json?planVersion={1}'.format(workitem_id, plan_version)
