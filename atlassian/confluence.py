73c2542720a4b1ff29f5f992af9876788a2db564
Matt Harasymczuk <matt@harasymczuk.pl>
2015-03-27 14:59:24 +0100

    PYAPI-35 Confluence: update_or_create

--- atlassian/confluence.py
+++ atlassian/confluence.py
@@ -11,21 +11,31 @@ class Confluence(AtlassianRestAPI):
     def page_exists(self, space, title):
         try:
             self.get_page_by_title(space, title)
+            log.info('Page "{title}" already exists in space "{space}"'.format(space=space, title=title))
             return True
         except (HTTPError, KeyError, IndexError):
+            log.info('Page "{title}" does not exist in space "{space}"'.format(space=space, title=title))
             return False
+
+    def get_page_id(self, space, title):
+        return self.get_page_by_title(space, title).get('id')
+
+    def get_page_space(self, page_id):
+        return self.get_page_by_id(page_id, expand='space')['space']['key']
 
     def get_page_by_title(self, space, title):
         url = '/rest/api/content?spaceKey={space}&title={title}'.format(space=space, title=title)
         return self.get(url)['results'][0]
 
-    def get_page_id(self, space, title):
-        return self.get_page_by_title(space, title).get('id')
+    def get_page_by_id(self, page_id, expand=None):
+        url = '/rest/api/content/{page_id}?expand={expand}'.format(page_id=page_id, expand=expand)
+        return self.get(url)
 
-    def create_page(self, space, parent_id, title, body):
+    def create_page(self, space, parent_id, title, body, type='page'):
+        log.info('Creating {type} "{space}" -> "{title}"'.format(space=space, title=title, type=type))
         return self.post('/rest/api/content/', data={
-            'type': 'page',
-            'ancestors': [{'type': 'page', 'id': parent_id}],
+            'type': type,
+            'ancestors': [{'type': type, 'id': parent_id}],
             'title': title,
             'space': {'key': space},
             'body': {'storage': {
@@ -35,8 +45,9 @@ class Confluence(AtlassianRestAPI):
     def history(self, page_id):
         return self.get('/rest/api/content/{0}/history'.format(page_id))
 
-    def update_page(self, parent_id, page_id, title, body, type='page',):
+    def update_page(self, parent_id, page_id, title, body, type='page'):
         version = self.history(page_id)['lastUpdated']['number'] + 1
+        log.info('Updating {type} "{title}"'.format(title=title, type=type))
         return self.put('/rest/api/content/{0}'.format(page_id), data={
             'id': page_id,
             'type': type,
@@ -46,3 +57,18 @@ class Confluence(AtlassianRestAPI):
                 'value': body,
                 'representation': 'storage'}},
             'version': {'number': version}})
+
+    def update_or_create(self, parent_id, title, body):
+        space = self.get_page_space(parent_id)
+
+        if self.page_exists(space, title):
+            page_id = self.get_page_id(space, title)
+            result = self.update_page(parent_id, page_id, title, body)
+        else:
+            result = self.create_page(space, parent_id, title, body)
+
+        log.warning('You may access your page at: {host}{url}'.format(
+            host=self.url,
+            url=result['_links']['tinyui']))
+
+        return result
