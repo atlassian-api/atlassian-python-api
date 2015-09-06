09a93873810ddb12bec4f82c366ea0a92d02d387
Jarek Śmiejczak <jot@smiejczak.net>
2015-09-06 02:44:28 +0200

    Implemented bamboo class

--- atlassian/bamboo.py
+++ atlassian/bamboo.py
@@ -1,52 +1,108 @@
 import logging
 from atlassian import AtlassianRestAPI
 
-
 log = logging.getLogger('atlassian.stash')
 
 
-class Stash(AtlassianRestAPI):
+class Bamboo(AtlassianRestAPI):
 
-	def projects_list(self, expand=None, favourite=False, cloverEnabled=False, start=0, limit=25):
-		args = ()
-		kwargs = {}
-		return self.get(self.resource_url('project'), args=args, kwargs=kwargs)
+    def base_list_call(self, resource, expand, favourite, cloverEnabled, start_index, max_results, **kwargs):
+        flags = []
+        params = {'start-index': start_index, 'max-results': max_results}
+        if expand:
+            params['expand'] = expand
+        if favourite:
+            flags.append('favourite')
+        if cloverEnabled:
+            flags.append('cloverEnabled')
+        params.update(kwargs)
+        return self.get(self.resource_url(resource), flags=flags, params=params)
 
-	def plans_list(self, expand=None, favourite=False, cloverEnabled=False, start=0, limit=25):
-		pass
+    def projects(self, expand=None, favourite=False, cloverEnabled=False, start_index=0, max_results=25):
+        return self.base_list_call('project', expand, favourite, cloverEnabled, start_index, max_results)
 
-	def result_list(self, project_key=None, plan_key=None, build_number=None,
-		expand=None, favourite=False, cloverEnabled=False, label=None,
-		start=0, limit=25):
-		pass
+    def plans(self, expand=None, favourite=False, cloverEnabled=False, start_index=0, max_results=25):
+        return self.base_list_call("plan", expand, favourite, cloverEnabled, start_index, max_results)
 
-	def get_project_latest_results(self, project, expand=None, favourite=False, cloverEnabled=False, label=None,
-		start=0, limit=25):
-		return self.result_list(project_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
-			label=label, start=start, limit=limit)
+    def results(self, project_key=None, plan_key=None, build_number=None, expand=None, favourite=False,
+                cloverEnabled=False, label=None, issueKey=None, start_index=0, max_results=25):
+        resource = "result"
+        if project_key and plan_key and build_number:
+            resource += "/{}-{}/{}".format(project_key, plan_key, build_number)
+        elif project_key and plan_key:
+            resource += "/{}-{}".format(project_key, plan_key)
+        elif project_key:
+            resource += '/' + project_key
 
-	def get_plan_results(self, project_key, plan_key, expand=None, favourite=False, cloverEnabled=False, label=None,
-		start=0, limit=25):
-		return self.result_list(project_key, plan_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
-			label=label, start=start, limit=limit)
+        params = {}
+        if issueKey:
+            params['issueKey'] = issueKey
+        return self.base_list_call(resource, expand, favourite, cloverEnabled, start_index, max_results, **params)
 
-	def get_result(self, project_key, plan_key, build_key, expand=None, favourite=False, cloverEnabled=False, label=None,
-		start=0, limit=25):
-		return self.result_list(project_key, plan_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
-			label=label, start=start, limit=limit)
+    def latest_results(self, expand=None, favourite=False, cloverEnabled=False, label=None, issueKey=None,
+                           start_index=0, max_results=25):
+        return self.results(expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
+                                label=label, issueKey=issueKey, start_index=start_index, max_results=max_results)
 
-	def chart_list(self, reportKey, buildKeys, groupByPeriod, dateFrom=None, dateTo=None,
-		width=None, height=None):
-		return self.result_list()
+    def project_latest_results(self, project_key, expand=None, favourite=False, cloverEnabled=False, label=None, issueKey=None,
+                                   start_index=0, max_results=25):
+        return self.results(project_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
+                                label=label, issueKey=issueKey, start_index=start_index, max_results=max_results)
 
-	def reports_list(self, expand=None, start=0, limit=25):
-		pass
+    def plan_results(self, project_key, plan_key, expand=None, favourite=False, cloverEnabled=False, label=None, issueKey=None,
+                         start_index=0, max_results=25):
+        return self.results(project_key, plan_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
+                                label=label, issueKey=issueKey, start_index=start_index, max_results=max_results)
 
-	def get_comments(self, project_key, plan_key, build_number):
-		pass
+    def build_result(self, project_key, plan_key, build_key, expand=None, favourite=False, cloverEnabled=False, label=None, issueKey=None,
+                   start_index=0, max_results=25):
+        return self.results(project_key, plan_key, expand=expand, favourite=favourite, cloverEnabled=cloverEnabled,
+                                label=label, issueKey=issueKey, start_index=start_index, max_results=max_results)
 
-	def get_labels(self, project_key, plan_key, build_number):
-		pass
+    def reports(self, expand=None, start_index=0, max_results=25):
+        params = {'start-index': start_index, 'max-results': max_results}
+        if expand:
+            params['expand'] = expand
 
-	def get_server_info(self):
-		pass
+        return self.get(self.resource_url('chart/reports'), params=params)
+
+    def chart(self, reportKey, buildKeys, groupByPeriod, dateFilter=None, dateFrom=None, dateTo=None,
+                   width=None, height=None, start_index=9, max_results=25):
+        params = {'reportKey': reportKey, 'buildKeys': buildKeys, 'groupByPeriod': groupByPeriod,
+                  'start-index': start_index, 'max-results': max_results}
+        if dateFilter:
+            params['dateFilter'] = dateFilter
+            if dateFilter == 'RANGE':
+                params['dateFrom'] = dateFrom
+                params['dateTo'] = dateTo
+        if width:
+            params['width'] = width
+        if height:
+            params['height'] = height
+        return self.get(self.resource_url('chart'), params=params)
+
+    def comments(self, project_key, plan_key, build_number, start_index=0, max_results=25):
+        resource = "result/{}-{}-{}/comment".format(project_key, plan_key, build_number)
+        params = {'start-index': start_index, 'max-results': max_results}
+        return self.get(self.resource_url(resource), params=params)
+
+    def create_comment(self, project_key, plan_key, build_number, comment, author=None):
+        resource = "result/{}-{}-{}/comment".format(project_key, plan_key, build_number)
+        comment_data = {'author': author if author else self.username, 'content': comment}
+        return self.post(self.resource_url(resource), data=comment_data)
+
+    def labels(self, project_key, plan_key, build_number, start_index=0, max_results=25):
+        resource = "result/{}-{}-{}/label".format(project_key, plan_key, build_number)
+        params = {'start-index': start_index, 'max-results': max_results}
+        return self.get(self.resource_url(resource), params=params)
+
+    def create_label(self, project_key, plan_key, build_number, label):
+        resource = "result/{}-{}-{}/label".format(project_key, plan_key, build_number)
+        return self.post(self.resource_url(resource), data={'name': label})
+
+    def delete_label(self, project_key, plan_key, build_number, label):
+        resource = "result/{}-{}-{}/label/{}".format(project_key, plan_key, build_number, label)
+        return self.delete(self.resource_url(resource))
+
+    def server_info(self):
+        return self.get(self.resource_url('info'))
