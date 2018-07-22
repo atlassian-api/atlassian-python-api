34eb74a758b8c27cc47a4932497832777d2ad242
Matt Harasymczuk <github.com@haras.pl>
2018-07-22 13:37:04 +0200

    Jira Project Administrator example cleanup

--- examples/jira-project-administrators.py
+++ examples/jira-project-administrators.py
@@ -1,11 +1,5 @@
-import logging
 from atlassian import Confluence
 from atlassian import Jira
-from pprint import pprint
-
-logging.basicConfig(level=logging.DEBUG, format='[%(asctime).19s] [%(levelname)s] %(message)s')
-logging.getLogger('requests').setLevel(logging.WARNING)
-log = logging.getLogger('jira-projects-administrators')
 
 
 jira = Jira(
@@ -20,8 +14,9 @@ html = """<table>
                     <th>Leader</th>
                     <th>Email</th>
                 </tr>"""
+
+
 for data in jira.project_leaders():
-    log.info('{project_key} leader is {lead_name} <{lead_email}>'.format(**data))
     html += """<tr>
                     <td>{project_key}</td>
                     <td>{project_name}</td>
@@ -29,6 +24,6 @@ for data in jira.project_leaders():
                     <td><a href="mailto:{lead_email}">{lead_email}</a></td>
                 </tr>""".format(**data)
 
-html += '</table><p></p><p></p>'
+html += '</table>'
 
-pprint(html)
+print(html)
