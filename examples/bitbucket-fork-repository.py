fd5fa92bb2645ab8c455a2038b4452204905a237
Spitzli Markus, INI-DOS-DPS-EAS <Markus.Spitzli@swisscom.com>
2018-12-21 21:14:36 +0100

    new feature: fork repository

---
+++ examples/bitbucket-fork-repository.py
@@ -0,0 +1,17 @@
+# coding: utf8
+from atlassian import Bitbucket
+
+bitbucket = Bitbucket(
+    url='http://localhost:7990',
+    username='admin',
+    password='admin')
+
+
+data = bitbucket.fork_repository(
+    project='DEMO',
+    repository='example-repository',
+    new_repository='forked-repository')
+
+
+
+print(data)
