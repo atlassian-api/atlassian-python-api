a8e5592969757219ff291e8ed18b5f4766e6f74b
Gonchik Tsymzhitov <Gonchik.tsymzhitov@itiviti.com>
2019-01-21 21:32:47 +0300

    Bibucket: Wrap method Get all repo list

--- atlassian/bitbucket.py
+++ atlassian/bitbucket.py
@@ -129,6 +129,25 @@ class Bitbucket(AtlassianRestAPI):
             log.info('Next page start at {}'.format(response.get('nextPageStart')))
         return (response or {}).get('values')
 
+    def repo_all_list(self, project_key):
+        """
+        Get all repositories list from project
+        :param project_key:
+        :return:
+        """
+        url = 'rest/api/1.0/projects/{projectKey}/repos'.format(projectKey=project_key)
+        params = {}
+        start = 0
+        params['start'] = start
+        response = self.get(url, params=params)
+        repo_list = (response or {}).get('values')
+        while not response.get('isLastPage'):
+            start = response.get('nextPageStart')
+            params['start'] = start
+            response = self.get(url, params=params)
+            repo_list += (response or {}).get('values')
+        return repo_list
+
     def get_branches(self, project, repository, base=None, filter=None, start=0, limit=99999, details=True,
                      order_by='MODIFICATION'):
         """
