b73a5a123fc9d2dfaeabdf400019bfefdee04383
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2019-10-16 09:47:06 -0400

    Added create_custom_field
    Added a function to create a custom field

--- atlassian/jira.py
+++ atlassian/jira.py
@@ -577,6 +577,15 @@ class Jira(AtlassianRestAPI):
         if limit:
             params['maxResults'] = limit
         return self.get(url, params=params)
+    
+    def create_custom_field(self, name, type, search_key=None, description=None):
+        url = 'rest/api/2/field'
+        data = {'name': name, 'type': type}
+        if search_key:
+            data['search_key'] = search_key
+        if description:
+            data['description'] = description
+        return self.post(url, data=data)
 
     def get_all_available_screen_fields(self, screen_id):
         """
