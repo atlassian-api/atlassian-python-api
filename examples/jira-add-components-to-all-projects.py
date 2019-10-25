21c3a1882324ad51bb8c3ebf892093a4acded615
Copolycube <kaourintin+github@gmail.com>
2019-10-25 11:38:56 -0400

    Update jira-add-components-to-all-projects.py
    correction on wrong variables + adding the option to skip some projects

--- examples/jira-add-components-to-all-projects.py
+++ examples/jira-add-components-to-all-projects.py
@@ -8,11 +8,17 @@ jira = Jira(
     
 components = ["Data Base", "HTML", "JavaScript"]
 
-"""That example show how to create components on all existing projects"""
+"""That example show how to create components on all existing projects, only skipping the one in a provided list"""
 
+project_to_skip = [ "SI", "SA", "ETA"]
+                   
 for i in jira.get_all_projects(included_archived=None):
-    for j in compo:
-        data = {"project": i["key"], "name":j}
-        jira.create_component(data)
-        print("{} - component created ".format(component.get('name')))
+    if i["key"] in project_to_skip :
+        print("Skipping project {} ".format(i["key"]))
+    else :
+        for j in components:
+            print("Creating in project {} ".format(i["key"]))
+            comp = {"project": i["key"], "name":j}
+            jira.create_component(comp)
+            print("{} - component created ".format(comp.get('name')))
         
