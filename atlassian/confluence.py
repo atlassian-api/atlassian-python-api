f9ba2d33147611ba156841f6b39ee47757e7dda3
Gonchik Tsymzhitov <Gonchik.tsymzhitov@itiviti.com>
2019-11-26 23:55:55 +0300

    Provide 2 methods with semantic correct naming

--- atlassian/confluence.py
+++ atlassian/confluence.py
@@ -104,21 +104,40 @@ class Confluence(AtlassianRestAPI):
         """
         return ((self.get_page_by_id(page_id, expand='space') or {}).get('space') or {}).get('key')
 
-    def get_page_by_title(self, space, title, expand=None):
+    def get_pages_by_title(self, space, title, start=0, limit=200, expand=None):
         """
-        Returns the list of labels on a piece of Content.
+        Provide pages by title search
         :param space: Space key
         :param title: Title of the page
+        :param start: OPTIONAL: The start point of the collection to return. Default: None (0).
+        :param limit: OPTIONAL: The limit of the number of labels to return, this may be restricted by
+                            fixed system limits. Default: 200.
+        :param expand: OPTIONAL: expand e.g. history
+        :return: The JSON data returned from searched results the content endpoint, or the results of the
+                 callback. Will raise requests.HTTPError on bad input, potentially.
+                 If it has IndexError then return the None.
+        """
+        return self.get_page_by_title(space, title, start, limit, expand)
+
+    def get_page_by_title(self, space, title, start=0, limit=1, expand=None):
+        """
+        Returns the first page  on a piece of Content.
+        :param space: Space key
+        :param title: Title of the page
+        :param start: OPTIONAL: The start point of the collection to return. Default: None (0).
+        :param limit: OPTIONAL: The limit of the number of labels to return, this may be restricted by
+                            fixed system limits. Default: 1.
         :param expand: OPTIONAL: expand e.g. history
         :return: The JSON data returned from searched results the content endpoint, or the results of the
                  callback. Will raise requests.HTTPError on bad input, potentially.
                  If it has IndexError then return the None.
         """
         url = 'rest/api/content'
-        params = {
-            'start': '0',
-            'limit': '1'
-        }
+        params = {}
+        if start is not None:
+            params['start'] = int(start)
+        if limit is not None:
+            params['limit'] = int(limit)
         if expand is not None:
             params['expand'] = expand
         if space is not None:
