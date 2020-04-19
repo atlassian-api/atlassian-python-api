2971335f972ef09c0c35caa17b7660a07a6aecc3
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2020-04-19 16:31:26 +0300

    Polish

--- atlassian/errors.py
+++ atlassian/errors.py
@@ -3,14 +3,18 @@
 class ApiError(Exception):
     pass
 
+
 class ApiNotFoundError(ApiError):
     pass
+
 
 class ApiPermissionError(ApiError):
     pass
 
+
 class ApiValueError(ApiError):
     pass
+
 
 class ApiConflictError(ApiError):
     pass
