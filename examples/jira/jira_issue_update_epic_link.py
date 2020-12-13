7faebb602bf978385b011b77890add06716e2840
Isaac Philip <isaac.philip25@gmail.com>
2020-12-13 23:23:17 +0530

    black formatting changes to let black command succeed in CI tests

--- examples/jira/jira_issue_update_epic_link.py
+++ examples/jira/jira_issue_update_epic_link.py
@@ -10,17 +10,14 @@ update_issues = ["ARA-1233", "ARA-1234"]
 
 
 def main():
-    jira = Jira(url='https://jira.example.com/',
-                username='user',
-                password='pass123')
-    epic_link_custom_field_id = "customfield_1404"     # Epic Link Custom Field
+    jira = Jira(url="https://jira.example.com/", username="user", password="pass123")
+    epic_link_custom_field_id = "customfield_1404"  # Epic Link Custom Field
     target_epic_issue_key = "ARA-1314"
     for issue_key in update_issues:
-        issue_data = jira.update_issue_field(issue_key,
-                                             fields={epic_link_custom_field_id: target_epic_issue_key})
+        issue_data = jira.update_issue_field(issue_key, fields={epic_link_custom_field_id: target_epic_issue_key})
         print(f"updated for {issue_key}")
     print("done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
