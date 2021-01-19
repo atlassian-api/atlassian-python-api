ab36a18f7748985f0b47174af6e6b458e1932a93
Marco Obermeyer <marco@knuettel.de>
2021-01-19 07:30:35 +0100

    Update service_desk.py

--- atlassian/service_desk.py
+++ atlassian/service_desk.py
@@ -471,7 +471,7 @@ class ServiceDesk(AtlassianRestAPI):
         experimental_headers = self.experimental_headers.copy()
         del experimental_headers["Content-Type"]
         experimental_headers["X-Atlassian-Token"] = "no-check"
-            
+
         with open(filename, "rb") as file:
             result = self.post(path=url, headers=experimental_headers, files={"file": file}).get("temporaryAttachments")
             temp_attachment_id = result[0].get("temporaryAttachmentId")
