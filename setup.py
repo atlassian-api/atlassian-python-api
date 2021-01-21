861f9b76b2a0a45363c814ed17538fae986f9331
offa <bm-dev@yandex.com>
2021-01-21 17:04:20 +0000

    [REST Client] Kerberos authentication improvements (#692)
    * [rest_client] Kerberos authentication ported to 'requests-kerberos'.
    
    * [rest_client] Kerberos parameter marked as unused.
    
    * [doc] Kerberos code example updated.

--- setup.py
+++ setup.py
@@ -26,12 +26,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=["deprecated", "requests", "six", "oauthlib", "requests_oauthlib"],
-    extras_require={
-        "kerberos": [
-            'kerberos-sspi ; platform_system=="Windows"',
-            'kerberos ; platform_system!="Windows"',
-        ]
-    },
+    extras_require={"kerberos": ['requests-kerberos']},
     platforms="Platform Independent",
     classifiers=[
         "Development Status :: 4 - Beta",
