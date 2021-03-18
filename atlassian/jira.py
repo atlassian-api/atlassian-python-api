3b6aa186c0329f5ef4ada1960d50403da1371da2
rakesh556575 <rakeshsingh556575@gmail.com>
2021-03-18 10:23:50 +0530

    Bulk issue  filed update
    Bulk issue  filed update

--- atlassian/jira.py
+++ atlassian/jira.py
@@ -804,6 +804,20 @@ class Jira(AtlassianRestAPI):
 
     def update_issue_field(self, key, fields="*all"):
         return self.put("rest/api/2/issue/{0}".format(key), data={"fields": fields})
+    
+    def bulk_update_issue_filed(self,key_list,fields="*all"):
+        """
+        :param key_list=list of issues with common filed to be updated
+        :param fields: common fields to be updated
+        return Boolean True/False
+        """
+        try:
+            for key in key_list:
+                self.update_issue_field(key,fields=fields)
+        except :
+            return False
+        return True
+        
 
     def get_issue_labels(self, issue_key):
         """
