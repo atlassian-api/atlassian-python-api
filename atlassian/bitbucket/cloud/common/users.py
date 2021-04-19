1832039d8da66abfabd7b79a13ff8c30836cf803
Robin De Neef <deneefrobin@gmail.com>
2021-04-19 23:32:57 +0200

    [Bitbucket Cloud] Add avatar property to user object

--- atlassian/bitbucket/cloud/common/users.py
+++ atlassian/bitbucket/cloud/common/users.py
@@ -7,16 +7,25 @@ class User(BitbucketCloudBase):
 
     @property
     def display_name(self):
+        """ Display name used by Bitbucket Cloud """
         return str(self.get_data("display_name"))
 
     @property
     def nickname(self):
+        """ Username used by Bitbucket Cloud """
         return self.get_data("nickname")
 
     @property
     def account_id(self):
+        """ Account id used by Bitbucket Cloud """
         return self.get_data("account_id")
 
     @property
     def uuid(self):
+        """ User id used by Bitbucket Cloud """
         return self.get_data("uuid")
+
+    @property
+    def avatar(self):
+        """ URL to user avatar on Bitbucket Cloud """
+        return self.get_data("links")["avatar"]["href"]
