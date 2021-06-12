e961a1bac49e7dabe7e15872e3fc07430f20405c
Steven Swor <sworisbreathing@gmail.com>
2021-06-13 07:48:17 +1000

    Bitbucket: add support for downloading repository archives (#791)

---
+++ examples/bitbucket/bitbucket_download_repo_archive.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from atlassian import Bitbucket
+
+bitbucket = Bitbucket(url="http://localhost:7990", username="admin", password="admin")
+
+with open("archive.tgz", mode="wb") as dest_fd:
+    bitbucket.download_repo_archive(
+        project_key="DEMO",
+        repository_slug="example-repository",
+        at="master",
+        format="tar.gz",
+    )
