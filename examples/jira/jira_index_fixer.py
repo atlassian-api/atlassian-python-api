1cd408f364c4631c6c08dea64f338c1e618158c1
Gonchik Tsymzhitov <gonchik.tsymzhitov@itiviti.com>
2021-06-16 07:54:32 +0300

    Change the os version

--- examples/jira/jira_index_fixer.py
+++ examples/jira/jira_index_fixer.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from atlassian import Jira
+from pprint import pprint
 
 JIRA_NODE_URL = "JIRA_NODES_1"
 JIRA_LOGIN = "admin"
@@ -15,7 +17,9 @@ class IndexFixer(Jira):
 
 s = IndexFixer(url=JIRA_NODE_URL, username=JIRA_LOGIN, password=JIRA_PASSWD)
 response = s.index_checker()
-print(response)
+print("-" * 20)
+pprint(response)
+print("-" * 20)
 fixer = False
 index_orphant_issues_count = response["indexOrphansCount"]
 if index_orphant_issues_count > 0:
