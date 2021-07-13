90730e16cd0525659a33e31b022a743c2dceea06
Isaac Philip <isaac.philip25@gmail.com>
2021-07-14 03:48:37 +0530

    [JIRA] Documentation and example addition for getting all projects (#816)
    * Jira : Get All Project Issues example added
    
    Closes #815
    
    * Get all Project Issues docstring added
    
    Closes #815
    
    * black reformatting applied

---
+++ examples/jira/jira_issues_get_all_projects.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+"""
+    Getting Project Issue(s) example
+"""
+
+
+from atlassian import Jira
+
+
+jira = Jira(url="https://jirasite.co", username="ocean", password="seariver")
+
+
+if __name__ == "__main__":
+    # default will return 50 issues in ascending order
+    project_issues_default_50 = jira.get_all_project_issues(project="APA")
+    # We can increase the limit by specifying the limit
+    project_issues_100 = jira.get_all_project_issues(project="APA", limit=100)
+    # Specifying specific fields other than the default jira fields returned
+    project_issues_specific = jira.get_all_project_issues(project="APA", fields=["description", "summary"], limit=100)
