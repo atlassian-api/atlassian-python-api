b07da6ae96717a167b8a47b7f4c8fe76c0ce94cc
ezchi <enze.chi@gmail.com>
2021-08-21 16:46:28 +1000

    Handle paginate to get full list of projects (#845)

--- atlassian/bamboo.py
+++ atlassian/bamboo.py
@@ -594,10 +594,25 @@ class Bamboo(AtlassianRestAPI):
 
     def get_projects(self):
         """Method used to list all projects defined in Bamboo.
-        Projects without any plan are not listed by default, unless showEmpty query param is set to true."""
-        resource = "project?showEmpty"
-        for project in self.get(self.resource_url(resource)):
-            yield project
+        Projects without any plan are not listed."""
+        start_idx = 0
+        max_results = 25
+
+        while True:
+            resource = "project?start-index={}&max-result={}".format(start_idx, max_results)
+
+            r = self.get(self.resource_url(resource))
+
+            if r is None:
+                break
+
+            if start_idx > r["projects"]["size"]:
+                break
+
+            start_idx += max_results
+
+            for project in r["projects"]["project"]:
+                yield project
 
     def get_project(self, project_key):
         """Method used to retrieve information for project specified as project key.
