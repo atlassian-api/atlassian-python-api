fd635669f0039efce7eaf5852231acd74b1a0b89
Hunter Casten <41604962+enchantednatures@users.noreply.github.com>
2021-11-03 11:03:59 -0400

    remove trailing space

--- atlassian/jira.py
+++ atlassian/jira.py
@@ -758,7 +758,7 @@ class Jira(AtlassianRestAPI):
             params["expand"] = expand
         url = "rest/api/2/issue/createmeta?projectKeys={}".format(project)
         return self.get(url, params=params)
-    
+
     def issue_editmeta(self, key):
         base_url = self.resource_url("issue")
         url = f"{base_url}/{key}/editmeta"
