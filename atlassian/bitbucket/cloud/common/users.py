03467d5125a6468805e9e17f2a5cf4431a71c9d2
Frank Lichtenheld <frank@lichtenheld.com>
2022-01-14 12:38:42 +0100

    Bitbucket Cloud: Add support for commit/commits endpoint
    The API is a bit confusing since commits/<hash> does not
    give you a commit, but that commit's history. Need to use
    commit/<hash> to get a specific commit only.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- atlassian/bitbucket/cloud/common/users.py
+++ atlassian/bitbucket/cloud/common/users.py
@@ -20,3 +20,46 @@ class User(BitbucketCloudBase):
     @property
     def uuid(self):
         return self.get_data("uuid")
+
+
+class Participant(BitbucketCloudBase):
+    ROLE_REVIEWER = "REVIEWER"
+    ROLE_PARTICIPANT = "PARTICIPANT"
+    CHANGES_REQUESTED = "changes_requested"
+
+    def __init__(self, data, *args, **kwargs):
+        super(Participant, self).__init__(None, None, *args, data=data, expected_type="participant", **kwargs)
+
+    @property
+    def user(self):
+        """User object with user information of the participant."""
+        return User(None, self.get_data("user"), **self._new_session_args)
+
+    @property
+    def is_participant(self):
+        """True if the user is a pull request participant."""
+        return self.get_data("role") == self.ROLE_PARTICIPANT
+
+    @property
+    def is_reviewer(self):
+        """True if the user is a pull request reviewer."""
+        return self.get_data("role") == self.ROLE_REVIEWER
+
+    @property
+    def is_default_reviewer(self):
+        """True if the user is a default reviewer."""
+
+    @property
+    def has_changes_requested(self):
+        """True if user requested changes."""
+        return str(self.get_data("state")) == self.CHANGES_REQUESTED
+
+    @property
+    def has_approved(self):
+        """True if user approved the pull request."""
+        return self.get_data("approved")
+
+    @property
+    def participated_on(self):
+        """Time of last participation."""
+        return self.get_time("participated_on")
