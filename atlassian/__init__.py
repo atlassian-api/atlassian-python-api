3780ba6f7f5061a1b7cbd57a4b9ae1e7d7c90d60
YugoHino <henom06@gmail.com>
2022-08-21 19:15:32 +0900

    Add new module Cloud Admin (#1033)
    * add Cloud Admin module
    
    * formatting
    
    * add examples
    
    * fix comment syntax

--- atlassian/__init__.py
+++ atlassian/__init__.py
@@ -1,20 +1,22 @@
 from .bamboo import Bamboo
 from .bitbucket import Bitbucket
 from .bitbucket import Bitbucket as Stash
+from .cloud_admin import CloudAdminOrgs, CloudAdminUsers
 from .confluence import Confluence
 from .crowd import Crowd
+from .insight import Insight
 from .jira import Jira
 from .marketplace import MarketPlace
 from .portfolio import Portfolio
 from .service_desk import ServiceDesk
 from .xray import Xray
-from .insight import Insight
-
 
 __all__ = [
     "Confluence",
     "Jira",
     "Bitbucket",
+    "CloudAdminOrgs",
+    "CloudAdminUsers",
     "Portfolio",
     "Bamboo",
     "Stash",
