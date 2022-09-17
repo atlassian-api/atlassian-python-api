a11e74ebb2122b82d2cb778572ccf6e1bdcdde8d
tuxmaster <tuxmaster@users.noreply.github.com>
2022-09-17 17:01:26 +0200

    Switch from SHA1 to SHA2

--- atlassian/rest_client.py
+++ atlassian/rest_client.py
@@ -3,7 +3,7 @@ import logging
 from json import dumps
 
 import requests
-from oauthlib.oauth1 import SIGNATURE_RSA
+from oauthlib.oauth1 import SIGNATURE_RSA_SHA512
 from requests import HTTPError
 from requests_oauthlib import OAuth1, OAuth2
 from six.moves.urllib.parse import urlencode
@@ -103,7 +103,7 @@ class AtlassianRestAPI(object):
         oauth = OAuth1(
             oauth_dict["consumer_key"],
             rsa_key=oauth_dict["key_cert"],
-            signature_method=SIGNATURE_RSA,
+            signature_method=SIGNATURE_RSA_SHA512,
             resource_owner_key=oauth_dict["access_token"],
             resource_owner_secret=oauth_dict["access_token_secret"],
         )
