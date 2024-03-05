e0a7c6f26d105e5f6d89d629f9a52883183eec34
Ermanno Baschiera <ebaschiera@gmail.com>
2024-03-05 22:17:47 +0100

    [Jira] fix paginated projects retrieving (#1343)
    * fix paginated projects retrieving
    
    Fixes #1310
    
    * fix indentation
    
    * fix format

--- atlassian/jira.py
+++ atlassian/jira.py
@@ -2324,14 +2324,17 @@ class Jira(AtlassianRestAPI):
             included_archived=included_archived,
             expand=expand,
         )
-        while not projects.get("isLast"):
-            projects["values"].extend(
-                self.paginated_projects(
-                    included_archived=included_archived,
-                    expand=expand,
-                    url=projects["nextPage"],
-                )["values"]
+        is_last_page = projects.get("isLast")
+        next_page_url = projects.get("nextPage")
+        while not is_last_page:
+            next_page_projects = self.paginated_projects(
+                included_archived=included_archived,
+                expand=expand,
+                url=next_page_url,
             )
+            next_page_url = next_page_projects.get("nextPage")
+            is_last_page = next_page_projects.get("isLast")
+            projects["values"].extend(next_page_projects["values"])
         return projects["values"]
 
     def paginated_projects(self, included_archived=None, expand=None, url=None):
