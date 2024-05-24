e25fc060399dbf6855491f91424850c1c4e1c771
ChowRex <zrx879582094@gmail.com>
2024-05-24 16:40:51 +0800

    Add `Crowd` a new attr
    Add a new property of class `Crowd`, can retrieves full details of all group memberships, see: https://docs.atlassian.com/atlassian-crowd/5.3.1/REST/#usermanagement/1/group-getAllMemberships

--- atlassian/crowd.py
+++ atlassian/crowd.py
@@ -264,3 +264,21 @@ class Crowd(AtlassianRestAPI):
         url = "/plugins/1.0/{plugin_key}/license".format(plugin_key=plugin_key)
         data = {"rawLicense": raw_license}
         return self.put(url, data=data, headers=app_headers)
+
+    @property
+    def memberships(self):
+        """
+        Retrieves full details of all group memberships, with users and nested groups.
+        See: https://docs.atlassian.com/atlassian-crowd/5.3.1/REST/#usermanagement/1/group-getAllMemberships
+        :return: All membership mapping dict
+        """
+        path = self._crowd_api_url("usermanagement", "group/membership")
+        headers = {'Accept': 'application/xml'}
+        response = self.get(path, headers=headers, advanced_mode=True)
+        root = etree.fromstring(response.content)
+        memberships = {}
+        for member in root.xpath('//membership'):
+            group = member.get('group')
+            users = [user.get('name') for user in member.xpath('./users/user')]
+            memberships[group] = users
+        return memberships
