ecf109fd9a225be2daf79a1498fa7ed3e45c6413
Gonchik Tsymzhitov <gonchik.tsymzhitov@atlassiancommunity.com>
2025-01-08 11:06:26 +0200

    confluence: wrap black formatter

--- atlassian/confluence.py
+++ atlassian/confluence.py
@@ -146,23 +146,23 @@ class Confluence(AtlassianRestAPI):
         else:
             return False
 
-    def share_with_others(self,page_id, group, message):
+    def share_with_others(self, page_id, group, message):
         """
         Notify members (currently only groups implemented) about something on that page
         """
         url = "rest/share-page/latest/share"
         params = {
             "contextualPageId": page_id,
-            #"emails": [],
+            # "emails": [],
             "entityId": page_id,
             "entityType": "page",
             "groups": group,
-            "note": message
-            #"users":[]
+            "note": message,
+            # "users":[]
         }
-        r = self.post(url, json=params,headers={"contentType":"application/json; charset=utf-8"},advanced_mode=True)
+        r = self.post(url, json=params, headers={"contentType": "application/json; charset=utf-8"}, advanced_mode=True)
         if r.status_code != 200:
-            raise Exception("failed sharing content {code}: {reason}".format(code=r.status_code,reason=r.text))
+            raise Exception("failed sharing content {code}: {reason}".format(code=r.status_code, reason=r.text))
 
     def get_page_child_by_type(self, page_id, type="page", start=None, limit=None, expand=None):
         """
