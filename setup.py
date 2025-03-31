b9b3f9e80b08682024fa12c6855e80a3f0bd938a
tuxmaster5000 <837503+tuxmaster5000@users.noreply.github.com>
2025-03-31 07:51:42 +0200

    Fix for GH-1517

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     package_dir={"atlassian": "atlassian"},
     include_package_data=True,
     zip_safe=False,
-    install_requires=["deprecated", "requests", "six", "oauthlib", "requests_oauthlib", "jmespath", "beautifulsoup4"],
+    install_requires=["deprecated", "requests", "six", "oauthlib", "requests_oauthlib", "jmespath", "beautifulsoup4", "typing-extensions"],
     extras_require={"kerberos": ["requests-kerberos"]},
     platforms="Platform Independent",
     classifiers=[
