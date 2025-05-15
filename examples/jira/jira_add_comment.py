34b6d6a009ebe5331db3bad1557c863de3457f17
Dima Unterov <dimaunterov@gmail.com>
2025-05-15 14:55:44 +0100

    chore: add an example to jira_add_comment.py

--- examples/jira/jira_add_comment.py
+++ examples/jira/jira_add_comment.py
@@ -6,3 +6,14 @@ from atlassian import Jira
 jira = Jira(url="https://jira.example.com/", username="gonchik.tsymzhitov", password="admin")
 
 jira.issue_add_comment("TST-11098", "test rest api request")
+
+""" How to add comment with link to another comment """
+
+issue_id = "TST-101"
+comment_id = "10001"
+
+new_comment_message = (
+    f"Answering to [comment #{comment_id}|{jira.url}browse/{issue_id}?focusedCommentId={comment_id}] ..."
+)
+
+jira.issue_add_comment(issue_id, new_comment_message)
