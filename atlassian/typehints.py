18e3dd3a835142a1cba6a0c86b2529a64475f9ec
Benjamin <43859462+halfow@users.noreply.github.com>
2026-08-03 14:51:32 +0200

    Improve typing experience of Jira instanziation (#1644)

--- atlassian/typehints.py
+++ atlassian/typehints.py
@@ -1,7 +1,13 @@
-from typing import Union
+from typing import Union, TypeVar, Callable, Any
 
 from typing_extensions import TypeAlias
 
 T_id: TypeAlias = Union[str, int]
 _Data: TypeAlias = Union[dict, str]
 T_resp_json: TypeAlias = Union[dict, None]
+
+_T = TypeVar("T")
+
+def copy_type(_:_T) -> Callable[[Any], _T]:
+    """Decorator to inherit typing from parent."""
+    return lambda x: x
